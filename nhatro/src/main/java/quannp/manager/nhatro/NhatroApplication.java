fbfc40b63997c2efd9471c46de4ad0fbc0b5b820
Quannp <npquan1995@gmail.com>
2025-01-29 00:12:43 +0700

    Init Commit

---
+++ nhatro/src/main/java/quannp/manager/nhatro/NhatroApplication.java
@@ -0,0 +1,13 @@
+package quannp.manager.nhatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhatroApplication.class, args);
+	}
+
+}
